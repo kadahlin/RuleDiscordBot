07a599b9619af51984507cb1049718e606f52816
Kyle Dahlin <kyle.dahlin0@gmail.com>
2020-07-30 20:34:47 -0700

    Changing the rules please display for readability, now split into two commands

--- rulebot/src/main/kotlin/com/kyledahlin/rulebot/RuleManager.kt
+++ rulebot/src/main/kotlin/com/kyledahlin/rulebot/RuleManager.kt
@@ -19,7 +19,6 @@ import com.kyledahlin.rulebot.bot.*
 import discord4j.core.`object`.entity.Guild
 import discord4j.core.`object`.entity.Member
 import discord4j.core.`object`.entity.MessageChannel
-import discord4j.core.`object`.util.Snowflake
 import discord4j.core.event.domain.message.MessageCreateEvent
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
@@ -32,7 +31,10 @@ import javax.inject.Inject
 private const val RULES = "rules please"
 
 @RuleBotScope
-internal class RuleManager @Inject constructor(rules: @JvmSuppressWildcards Set<Rule>, private val cache: DiscordCache) {
+internal class RuleManager @Inject constructor(
+    rules: @JvmSuppressWildcards Set<Rule>,
+    private val cache: DiscordCache
+) {
 
     private var _rules: List<Rule> = rules.sortedBy { it.priority.ordinal }
 
@@ -52,15 +54,26 @@ internal class RuleManager @Inject constructor(rules: @JvmSuppressWildcards Set<
         GlobalScope.launch {
             Logger.logDebug("got message event $messageEvent")
             val (messageCreated, guild, channel, member) = convertMessageCreateEvent(messageEvent)
-            cache.createEventWrapperEntry(messageCreated as RuleBotEvent, channel as MessageChannel, guild as Guild, member as Member)
+            cache.createEventWrapperEntry(
+                messageCreated as RuleBotEvent,
+                channel as MessageChannel,
+                guild as Guild,
+                member as Member
+            )
             _rules.any {
                 Logger.logDebug("handling messaging for ${it.ruleName}")
                 val wasHandled = it.handleEvent(messageCreated)
                 Logger.logDebug("message was ${if (wasHandled) "" else "not "}handled by ${it.ruleName}")
                 wasHandled
             }
-            if (messageEvent.message.content.get() == RULES) {
-                messageEvent.message.channel.awaitFirstOrNull()?.printRules()
+            val content = messageEvent.message.content.get()
+            if (content.startsWith(RULES)) {
+                val contentPieces = messageEvent.message.content.get().split(" ")
+                if (contentPieces.size > 2) {
+                    messageEvent.message.channel.awaitFirstOrNull()?.printExplanationOfRule(contentPieces[2])
+                } else {
+                    messageEvent.message.channel.awaitFirstOrNull()?.printRules()
+                }
             }
         }
     }
@@ -81,8 +94,20 @@ internal class RuleManager @Inject constructor(rules: @JvmSuppressWildcards Set<
     private suspend fun MessageChannel.printRules() {
         val ruleMessages = _rules
             .filterNot { it.getExplanation() == null }
-            .joinToString(separator = "\n") { "${it.ruleName}:\t${it.getExplanation()}" }
+            .sortedBy { it.ruleName }
+            .mapIndexed { index, rule -> "${index}. ${rule.ruleName}" }
+            .joinToString(separator = "\n")
+
         suspendCreateMessage(ruleMessages)
+    }
+
+    private suspend fun MessageChannel.printExplanationOfRule(name: String) {
+        val ruleMessage = _rules.firstOrNull { it.ruleName == name }?.getExplanation()
+        if (ruleMessage == null) {
+            suspendCreateMessage("No rule with that name")
+        } else {
+            suspendCreateMessage(ruleMessage)
+        }
     }
 
     internal suspend fun getRuleNames(): Set<String> {
