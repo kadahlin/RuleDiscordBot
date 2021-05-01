571880ed9ebb91cfab7411544cefb9afbda4b3d0
Kyle Dahlin <kyle.dahlin0@gmail.com>
2021-05-01 11:25:43 -0700

    travis

--- start.sh
+++ start.sh
@@ -12,4 +12,4 @@ docker tag ${IMAGE}:${GIT_VERSION} ${IMAGE}:latest
 echo "${DOCKER_PASSWORD}" | docker login -u "${DOCKER_USERNAME}" --password-stdin
 docker push ${IMAGE}:${GIT_VERSION}
 
-doctl compute ssh -t ${DIGITAL_TOKEN} ubuntu-honkbot --ssh-command "docker stop rulebot ; docker rm rulebot && docker run -p 6969:6969 -v /root:/mnt/kyledahlin --env HONKBOT_CREDENTIALS=/mnt/kyledahlin/honkbot-5c9e4-firebase-adminsdk-fe2wk-a12b80cdc2.json --env RULEBOT_HOST=0.0.0.0 -d --env HONKBOT_TOKEN=/mnt/kyledahlin/honkbottoken.txt --name=rulebot --restart unless-stopped ${IMAGE}:${GIT_VERSION} && docker system prune -a -f"
+doctl compute ssh -t ${DIGITAL_TOKEN} ubuntu-honkbot --ssh-command "docker stop rulebot ; docker rm rulebot ; docker run -p 6969:6969 -v /root:/mnt/kyledahlin --env HONKBOT_CREDENTIALS=/mnt/kyledahlin/honkbot-5c9e4-firebase-adminsdk-fe2wk-a12b80cdc2.json --env RULEBOT_HOST=0.0.0.0 -d --env HONKBOT_TOKEN=/mnt/kyledahlin/honkbottoken.txt --name=rulebot --restart unless-stopped ${IMAGE}:${GIT_VERSION} && docker system prune -a -f"
